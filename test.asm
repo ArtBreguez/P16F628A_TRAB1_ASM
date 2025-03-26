ac6678f3cd40adf0d84dba4100ffe31b3693b065
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2025-03-26 20:53:33 -0300

    feat: inicializando input value e setando quando os botões forem pressionados

--- test.asm
+++ test.asm
@@ -1,61 +1,69 @@
 ; ******************************************************************************
-; * Algoritmo de Controle de LEDs - PIC16F628A                                  *
-; * Autor: ARTHUR G. BREGUEZ                     Data: 22/03/2025               *
+; * Algoritmo de Controle de LEDs - PIC16F628A                                 *
+; * Autor: ARTHUR G. BREGUEZ  & JOAO VICTOR      Data: 26/03/2025              *
 ; ******************************************************************************
 
-#include <P16F628A.INC>                     ; Inclui definições do PIC16F628A
+#include <P16F628A.INC>                     ; Inclui definicoes do PIC16F628A
 
 ; ============================================================================== 
-; CONFIGURAÇÕES DO MICROCONTROLADOR
+; CONFIGURACOES DO MICROCONTROLADOR
 ; ==============================================================================
     __CONFIG _BODEN_ON & _CP_OFF & _PWRTE_ON & _WDT_OFF & _LVP_OFF & _MCLRE_ON & _HS_OSC
 
 ; ==============================================================================
-; DEFINIÇÕES DE BANCOS DE MEMÓRIA
+; DEFINICOES DE BANCOS DE MEMORIA
 ; ==============================================================================
 #define BANK0  BCF STATUS,RP0               ; Seleciona Banco 0
 #define BANK1  BSF STATUS,RP0               ; Seleciona Banco 1
 
 ; ============================================================================== 
-; VARIÁVEIS
+; VARIAVEIS
 ; ==============================================================================
-    CBLOCK 0x20                             ; Endereço inicial das variáveis
-        DELAY_VAR1                          ; Variável auxiliar para controle do loop externo do delay
-        DELAY_VAR2                          ; Variável auxiliar para controle do loop interno do delay
+    CBLOCK 0x20                             ; Endereco inicial das variaveis
+        DELAY_VAR1                          ; Variavel auxiliar para delays
+        DELAY_VAR2                          ; Variavel auxiliar para delays
+        INPUT_VALUE                         ; Armazena o valor de 3 bits lidos dos botoes
     ENDC
 
 ; ============================================================================== 
-; INÍCIO DO PROGRAMA
+; INICIO DO PROGRAMA
 ; ==============================================================================
     ORG 0x00                                ; Define origem do programa em 0x00
-    GOTO INICIO                             ; Pula para a rotina de inicialização
+    GOTO INICIO                             ; Pula para a rotina de inicializacao
 
 ; ============================================================================== 
 ; ROTINA PRINCIPAL
 ; ==============================================================================
 INICIO:
-    BANK1                                   ; Acessa Banco 1 para configurar os registradores
-    MOVLW   b'00001110'                     ; Configura RA1, RA2 e RA3 como entradas
-    MOVWF   TRISA                           ; Define os bits correspondentes do TRISA
-    MOVLW   b'00000000'                     ; Configura todas as portas de PORTB como saída
-    MOVWF   TRISB                           ; Define os bits correspondentes do TRISB
+    BANK1                                   ; Acessa Banco 1 para configuracao
+    MOVLW   b'00001110'                     ; RA1, RA2 e RA3 como entrada
+    MOVWF   TRISA                           
+    MOVLW   b'00000000'                     ; PORTB como saida
+    MOVWF   TRISB                           
     BANK0                                   ; Retorna ao Banco 0
-    CLRF    PORTB                           ; Garante que todos os LEDs estejam apagados inicialmente
+    CLRF    PORTB                           ; Inicializa PORTB em 0
+    CLRF    INPUT_VALUE                     ; Inicializa variavel de entrada como 0
 
     ; Acende LED RB0 e aguarda
     BSF     PORTB, 0                        ; Acende LED RB0
-    CALL    DELAY                           ; Chama a rotina de delay para esperar t segundos
+    CALL    DELAY                           ; Aguarda t segundos
     BCF     PORTB, 0                        ; Apaga LED RB0
-    
+    BTFSC   PORTA, 1                        ; Verifica se RA1 esta pressionado
+    BSF     INPUT_VALUE, 0                  ; Define bit 0 de INPUT_VALUE se RA1 estiver pressionado
+
     ; Acende LED RB1 e aguarda
     BSF     PORTB, 1                        ; Acende LED RB1
-    CALL    DELAY                           ; Chama a rotina de delay para esperar t segundos
+    CALL    DELAY                           ; Aguarda t segundos
     BCF     PORTB, 1                        ; Apaga LED RB1
-    
+    BTFSC   PORTA, 2                        ; Verifica se RA2 esta pressionado
+    BSF     INPUT_VALUE, 1                  ; Define bit 1 de INPUT_VALUE se RA2 estiver pressionado
+
     ; Acende LED RB2 e aguarda
     BSF     PORTB, 2                        ; Acende LED RB2
-    CALL    DELAY                           ; Chama a rotina de delay para esperar t segundos
+    CALL    DELAY                           ; Aguarda t segundos
     BCF     PORTB, 2                        ; Apaga LED RB2
+    BTFSC   PORTA, 3                        ; Verifica se RA3 esta pressionado
+    BSF     INPUT_VALUE, 2                  ; Define bit 2 de INPUT_VALUE se RA3 estiver pressionado
 
     GOTO    INICIO                          ; Reinicia o processo
 
@@ -63,16 +71,16 @@ INICIO:
 ; ROTINA DE DELAY (t segundos)
 ; ==============================================================================
 DELAY:
-    MOVLW   0xFF                            ; Carrega 255 no registrador W para definir o número de iterações do loop externo
-    MOVWF   DELAY_VAR1                      ; Armazena esse valor na variável DELAY_VAR1
+    MOVLW   0xFF                            ; Ajuste fino do tempo de espera
+    MOVWF   DELAY_VAR1                      
 DELAY_LOOP1:
-    MOVLW   0xFF                            ; Carrega 255 no registrador W para definir o número de iterações do loop interno
-    MOVWF   DELAY_VAR2                      ; Armazena esse valor na variável DELAY_VAR2
+    MOVLW   0xFF                            
+    MOVWF   DELAY_VAR2                      
 DELAY_LOOP2:
-    DECFSZ  DELAY_VAR2, F                   ; Decrementa DELAY_VAR2 e verifica se chegou a zero
-    GOTO    DELAY_LOOP2                     ; Se não chegou a zero, continua no loop interno
-    DECFSZ  DELAY_VAR1, F                   ; Decrementa DELAY_VAR1 e verifica se chegou a zero
-    GOTO    DELAY_LOOP1                     ; Se não chegou a zero, reinicia o loop externo
-    RETURN                                  ; Se ambos os loops terminarem, retorna para a rotina principal
+    DECFSZ  DELAY_VAR2, F                   
+    GOTO    DELAY_LOOP2                     
+    DECFSZ  DELAY_VAR1, F                   
+    GOTO    DELAY_LOOP1                     
+    RETURN                                  
 
     END                                     ; Fim do programa
